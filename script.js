060f2592c7a38b81c3b9b3e27888209138468a18
Shabin-Hussain <shabinhussain112@gmail.com>
2026-08-04 23:22:03 +0530

    initial version

---
+++ script.js
@@ -0,0 +1,22 @@
+VANTA.NET({
+
+    el: "#main",
+
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+
+    minHeight: 200,
+    minWidth: 200,
+
+    scale: 1,
+    scaleMobile: 1,
+
+    color: 0x3b82f6,
+    backgroundColor: 0x050816,
+
+    points: 12,
+    maxDistance: 22,
+    spacing: 18
+
+});
